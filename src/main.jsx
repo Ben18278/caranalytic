4d0f7dc9253e6401dd1ff4d34b0647ad2d69905d
Ben18278 <127289952+Ben18278@users.noreply.github.com>
2024-08-25 17:50:38 +0700

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { HashRouter, Route, Routes } from 'react-router-dom';
+import Dashboard from './pages/Dashboard.jsx';
+import HighlightedCarsPage from './pages/HighlightedCarsPage.jsx';
 import App from './App.jsx';
 import './index.css';
 
@@ -9,8 +11,8 @@ const rootElement = document.getElementById('root');
 const router = (
   <HashRouter>
     <Routes>
-      <Route path="/" element={<App />} />
-      {/* Add other routes here as needed */}
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/highlighted" element={<HighlightedCarsPage />} />
     </Routes>
   </HashRouter>
 );
